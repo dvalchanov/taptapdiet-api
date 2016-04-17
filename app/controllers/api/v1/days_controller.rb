1138d8d2d2042d1408e3b9b2f20df7fe6638573f
gavroshcho <nikolaivylchanov@gmail.com>
2016-04-17 10:49:38 +0300

    Make average

--- app/controllers/api/v1/days_controller.rb
+++ app/controllers/api/v1/days_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::DaysController < Api::BaseController
 	def index
     limit = (params[:limit] || 10).to_i
     offset = (params[:offset] || 0).to_i
-    @days = current_user.days.limit(limit).offset(offset)
+    @days = current_user.days.limit(limit).offset(offset).order(created_at: :desc)
 	end
 end
--- app/models/day.rb
+++ app/models/day.rb
@@ -2,5 +2,13 @@ class Day < ApplicationRecord
   belongs_to :user
   has_many :meals
   has_many :evaluations
+
+
+  def average_rating
+    ratings = evaluations.pluck(:rating)
+    size = ratings.size
+    sum = ratings.reduce(0, :+)
+    sum/(size-1)
+  end
 end
 

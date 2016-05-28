1022cab27b5e38a47c42bcc2b33f7c3e43f33eb7
gavroshcho <nikolaivylchanov@gmail.com>
2016-05-28 15:15:02 +0300

    Search for meal

--- app/controllers/api/v1/days_controller.rb
+++ app/controllers/api/v1/days_controller.rb
@@ -2,6 +2,9 @@ class Api::V1::DaysController < Api::BaseController
 	def index
     limit = (params[:limit] || 10).to_i
     offset = (params[:offset] || 0).to_i
-    @days = current_user.days.limit(limit).offset(offset).order(created_at: :desc)
+    if params[:title]
+      @days = current_user.days.limit(limit).offset(offset).ordered.search
+    else
+      @days = current_user.days.limit(limit).offset(offset).ordered
 	end
 end
--- app/models/day.rb
+++ app/models/day.rb
@@ -3,12 +3,18 @@ class Day < ApplicationRecord
   has_many :meals, dependent: :destroy
   has_many :feelings, dependent: :destroy
 
+  scope :ordered, -> { order(created_at: :desc) }
+
   def last_feeling
     if feelings.size != 0
       feelings.order(created_at: :asc).last.value
     else
       nil
     end
+  end
+
+  def search
+    @results = Meal.where("title ILIKE ?", params[:title])
   end
 
 end

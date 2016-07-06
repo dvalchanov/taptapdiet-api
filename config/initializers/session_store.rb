6cd692ed743b82932e8c4a43dfd8313a7647190c
gavroshcho <nikolaivylchanov@gmail.com>
2016-07-06 18:22:21 +0300

    Update to rails 5.0

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_taptapdiet-api_session'
+Rails.application.config.session_store :cookie_store, key: '_taptapdiet_api_session'

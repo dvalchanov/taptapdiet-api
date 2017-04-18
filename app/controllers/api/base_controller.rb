559954022a0d85f13b3d11c21dcb57743b717bfd
gavroshcho <nikolaivylchanov@gmail.com>
2017-04-18 15:23:48 +0300

    Fix tabulation

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,25 +1,24 @@
 class Api::BaseController < ApplicationController
-	BEARER = 'Bearer'.freeze
+  BEARER = 'Bearer'.freeze
 
-	before_action :authenticate!
+  before_action :authenticate!
 
-	private
+  private
 
-		def authenticate!
-			return if current_user.present?
-			render json: { error: 'UNAUTHORIZED', code: 401 }, status: :unauthorized
-		end
+  def authenticate!
+    render json: { error: 'UNAUTHORIZED', code: 401 }, status: :unauthorized
+  end
 
-		def current_user
-			@current_user.presence || authenticate_with_auth_token
-		end
+  def current_user
+    @current_user.presence || authenticate_with_auth_token
+  end
 
-		def authenticate_with_auth_token
-	    authorization = request.headers.fetch('HTTP_AUTHORIZATION') { '' }
-	    type, token = authorization.split(' ')
-	    return if type != BEARER
-	    access_token = AccessToken.find_by(token: token)
-	    return if access_token.blank? || access_token.expired?
-	    @current_user = access_token.user
-		end
+  def authenticate_with_auth_token
+    authorization = request.headers.fetch('HTTP_AUTHORIZATION') { '' }
+    type, token = authorization.split(' ')
+    return if type != BEARER
+    access_token = AccessToken.find_by(token: token)
+    return if access_token.blank? || access_token.expired?
+    @current_user = access_token.user
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,28 @@
 class User < ApplicationRecord
-	validates :fb_token, presence: true
+  validates :fb_token, presence: true
   validates :fb_user_id, presence: true
   validates :fb_token_expiration_date, presence: true
 
-	has_many :access_tokens
+  has_many :access_tokens
   has_many :days, dependent: :destroy
 
   after_create :create_first_day
 
-	class << self
-		def create_from_facebook!(params)
-		  user = find_or_initialize_by(fb_user_id: params[:fb_user_id])
-		  new_record = user.new_record?
+  class << self
+    def create_from_facebook!(params)
+      user = find_or_initialize_by(fb_user_id: params[:fb_user_id])
+      new_record = user.new_record?
 
       user.user_name = params[:user_name]
       
-		  user.time_zone = params[:time_zone]
-		  user.fb_token = params[:fb_token]
-		  user.fb_token_expiration_date = params[:fb_token_expiration_date]
-		  [user.tap(&:save!), new_record]
-		end
-	end
+      user.time_zone = params[:time_zone]
+      user.fb_token = params[:fb_token]
+      user.fb_token_expiration_date = params[:fb_token_expiration_date]
+      [user.tap(&:save!), new_record]
+    end
+  end
 
-	def create_first_day
+  def create_first_day
     days.create!
-	end
+  end
 end

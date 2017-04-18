f0d50f2e5a4ae347aa6fc5fd39866da756d42a77
gavroshcho <nikolaivylchanov@gmail.com>
2017-04-18 15:35:29 +0300

    Fix tabulation

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,22 +1,22 @@
 class AccessToken < ApplicationRecord
-	validates :token, presence: true
-	validates :expires_at, presence: true
+  validates :token, presence: true
+  validates :expires_at, presence: true
 
-	belongs_to :user
+  belongs_to :user
 
-	scope :by_date, -> { order(created_at: :desc) }
+  scope :by_date, -> { order(created_at: :desc) }
 
-	before_validation :assign_token, on: :create
+  before_validation :assign_token, on: :create
 
   def expired?
     expires_at < Time.zone.now
   end
 
-	
-	private
 
-	def assign_token
-	  self.token = SecureRandom.hex(32)
-	  self.expires_at = 1.year.from_now
+  private
+
+  def assign_token
+    self.token = SecureRandom.hex(32)
+    self.expires_at = 1.year.from_now
   end
 end
